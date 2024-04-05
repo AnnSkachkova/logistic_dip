8dcdab78c1eae70e18bba58c3b5839e017c84244
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2024-04-05 21:05:10 +0300

    модель доставки продуктов создана

--- apps/deliveries/models.py
+++ apps/deliveries/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from apps.products.models import Product
+# from accounts.models import Supplier  # Предполагая, что модель Supplier теперь в приложении accounts
 
-# Create your models here.
+
+class Delivery(models.Model):
+    STATUS_CHOICES = (
+        ('ordered', 'Ordered'),
+        ('shipped', 'Shipped'),
+        ('received', 'Received'),
+    )
+    # supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ordered')
+    expected_arrival = models.DateField()
+    actual_arrival = models.DateField(null=True, blank=True)
+
+class DeliveryProduct(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
