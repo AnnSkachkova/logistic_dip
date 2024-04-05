c98f52ed48bfb2b8440dab56ee2426617bc1d8fd
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2024-04-05 21:16:04 +0300

    приложения accounts создано

--- conf/urls.py
+++ conf/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.products.urls'), name='products'),
     path('orders/', include('apps.orders.urls'), name='orders'),
-    path('deliveries/', include('apps.deliveries.urls'), name='orders'),
-    
+    path('deliveries/', include('apps.deliveries.urls'), name='deliveries'),
+    path('accounts/', include('apps.accounts.urls'), name='accounts'),
 ]
