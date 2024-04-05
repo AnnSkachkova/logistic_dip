48eb82af9bd0d7309f77755152d3c43f6fc0d00f
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2024-04-05 20:45:06 +0300

    приложение deliveries создано

--- conf/urls.py
+++ conf/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.products.urls'), name='products'),
+    path('orders/', include('apps.orders.urls'), name='orders'),
+    path('deliveries/', include('apps.deliveries.urls'), name='orders'),
+    
 ]
