63bfc28047fb996798cc30e73342e77cf3cab457
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2024-04-07 12:22:02 +0300

    логика crud поставщиков сделана

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import SupplierListView, SupplierCreateView, SupplierUpdateView, SupplierDeleteView
 
-
-app_name = 'apps'
-
+app_name = 'accounts'
 
 urlpatterns = [
-    
+    path('suppliers/', SupplierListView.as_view(), name='supplier_list'),
+    path('suppliers/create/', SupplierCreateView.as_view(), name='supplier_create'),
+    path('suppliers/<int:pk>/update/', SupplierUpdateView.as_view(), name='supplier_update'),
+    path('suppliers/<int:pk>/delete/', SupplierDeleteView.as_view(), name='supplier_delete'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Supplier
+from .forms import SupplierForm 
 
-# Create your views here.
+
+class SupplierListView(ListView):
+    model = Supplier
+    context_object_name = 'suppliers'
+    template_name = 'accounts/supplier_list.html'
+
+
+class SupplierCreateView(CreateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = 'accounts/supplier_form.html'
+    success_url = reverse_lazy('accounts:supplier_list')
+
+
+class SupplierUpdateView(UpdateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = 'accounts/supplier_form.html'
+    success_url = reverse_lazy('accounts:supplier_list')
+
+
+class SupplierDeleteView(DeleteView):
+    model = Supplier
+    template_name = 'accounts/supplier_confirm_delete.html'
+    success_url = reverse_lazy('accounts:supplier_list')
